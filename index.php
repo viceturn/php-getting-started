ce3faeca413e327a1f66bd8e0aa46487b2188cf1
viceturn <vic86mut13@gmail.com>
2022-11-10 13:19:05 +0300

    first message

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "Hello world program";
+
+$my_name = "Victor Ndegwa" ;
+echo $my_name;
+var_dump($my_name);
+
+// integers
+$a = 30;
+$b = "30";
+$c = true;
+$d = 200.34;
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+
+echo "My Name is ".$my_name. "My M-PESA balance is 
+        Ksh. ".$d;
+?>
